fa0bc40a25f99f7171afa9cd868d18197f9694a8
David Acevedo <rabbit.editor@gmail.com>
2013-05-14 18:32:51 -0400

    adds puma as default server + i18n stuff

--- lib/i18n_preprocess.rb
+++
@@ -1,8 +0,0 @@
-require './helper'
-
-YAML.load('../locales/es_source.yaml')
-
-# load YAML doc
-# scroll through it and identify offending chars w/ html chars
-# replace characters
-# save new Yaml doc
